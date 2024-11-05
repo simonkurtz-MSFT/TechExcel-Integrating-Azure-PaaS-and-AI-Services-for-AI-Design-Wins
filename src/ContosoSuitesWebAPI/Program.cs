720caa102a08ccd4dfcfa7e5f549503d7504de63
Simon Kurtz <simonkurtz@microsoft.com>
2024-11-05 17:34:02 -0500

    Exercise 2-2

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -8,8 +8,16 @@ using Microsoft.Data.SqlClient;
 using Azure.AI.OpenAI;
 using Azure;
 using Microsoft.AspNetCore.Mvc;
+ using Microsoft.SemanticKernel;
+ using Microsoft.SemanticKernel.Connectors.OpenAI;
+ using Microsoft.SemanticKernel.ChatCompletion;
 
 var builder = WebApplication.CreateBuilder(args);
+
+ var config = new ConfigurationBuilder()
+     .AddUserSecrets<Program>()
+     .AddEnvironmentVariables()
+     .Build();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -20,6 +28,18 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IDatabaseService, DatabaseService>();
 builder.Services.AddSingleton<IVectorizationService, VectorizationService>();
 builder.Services.AddSingleton<MaintenanceCopilot, MaintenanceCopilot>();
+
+ builder.Services.AddSingleton<Kernel>((_) =>
+ {
+     IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+     kernelBuilder.AddAzureOpenAIChatCompletion(
+         deploymentName: builder.Configuration["AzureOpenAI:DeploymentName"]!,
+         endpoint: builder.Configuration["AzureOpenAI:Endpoint"]!,
+         apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!
+     );
+     kernelBuilder.Plugins.AddFromType<DatabaseService>();
+     return kernelBuilder.Build();
+ });
 
 // Create a single instance of the CosmosClient to be shared across the application.
 builder.Services.AddSingleton<CosmosClient>((_) =>
@@ -55,7 +75,7 @@ app.UseHttpsRedirection();
 // This endpoint serves as the default landing page for the API.
 app.MapGet("/", async () =>
 {
-    return "Welcome to the Contoso Suites Web API - 1!";
+    return "Welcome to the Contoso Suites Web API!";
 })
     .WithName("Index")
     .WithOpenApi();
@@ -90,13 +110,19 @@ app.MapGet("/", async () =>
 
 // This endpoint is used to send a message to the Azure OpenAI endpoint.
 app.MapPost("/Chat", async Task<string> (HttpRequest request) =>
-{
-    var message = await Task.FromResult(request.Form["message"]);
-
-    return "This endpoint is not yet available.";
-})
-    .WithName("Chat")
-    .WithOpenApi();
+ {
+     var message = await Task.FromResult(request.Form["message"]);
+     var kernel = app.Services.GetRequiredService<Kernel>();
+     var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+     var executionSettings = new OpenAIPromptExecutionSettings
+     {
+         ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+     };
+     var response = await chatCompletionService.GetChatMessageContentAsync(message.ToString(), executionSettings, kernel);
+     return response?.Content!;
+ })
+     .WithName("Chat")
+     .WithOpenApi();
 
 // This endpoint is used to vectorize a text string.
 // We will use this to generate embeddings for the maintenance request text.
